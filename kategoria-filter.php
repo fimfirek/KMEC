a0bd5c31e90cc876ba539028658177aec4e9801a
fimfirek <rmr.resell@gmail.com>
2024-06-04 10:57:47 +0200

    zoradenie

--- kategoria-filter.php
+++ kategoria-filter.php
@@ -6,6 +6,26 @@ $minPrice = isset($_GET['minPrice']) ? $_GET['minPrice'] : 0;
 $maxPrice = isset($_GET['maxPrice']) ? $_GET['maxPrice'] : 885.2;
 $category = isset($_GET['category']) ? $_GET['category'] : null;
 
+$sortOrder = isset($_GET['sortOrder']) ? $_GET['sortOrder'] : '';
+
+// Define the sorting order based on the sortOrder parameter
+$sortSql = '';
+switch($sortOrder) {
+    case 'alphabetical_asc':
+        $sortSql = ' ORDER BY title ASC';
+        break;
+    case 'alphabetical_desc':
+        $sortSql = ' ORDER BY title DESC';
+        break;
+    case 'price_asc':
+        $sortSql = ' ORDER BY price ASC';
+        break;
+    case 'price_desc':
+        $sortSql = ' ORDER BY price DESC';
+        break;
+}
+
+
 if (isset($_GET['minPrice']) || isset($_GET['maxPrice'])) {
     $sqlSelect = "SELECT * FROM produkty WHERE price BETWEEN ? AND ?";
     $stmt = mysqli_prepare($conn, $sqlSelect);
@@ -23,8 +43,8 @@ if ($category) {
 
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
-?>
 
+?>
 
 <div class="blur">
     <div class="filter-container">
@@ -33,6 +53,13 @@ $result = mysqli_stmt_get_result($stmt);
             <input type="number" id="minPrice" name="minPrice" min="0" step="0.50" value="<?php echo $minPrice; ?>">
             <label for="maxPrice">Maximálna cena:</label>
             <input type="number" id="maxPrice" name="maxPrice" min="0" step="0.50" value="<?php echo $maxPrice; ?>">
+            <select id="sortOrder" name="sortOrder">
+                <option value="">Vyberte</option>
+                <option value="alphabetical_asc" <?php if($sortOrder == 'alphabetical_asc') echo 'selected'; ?>>Od A po Z</option>
+                <option value="alphabetical_desc" <?php if($sortOrder == 'alphabetical_desc') echo 'selected'; ?>>Od Z po A</option>
+                <option value="price_asc" <?php if($sortOrder == 'price_asc') echo 'selected'; ?>>Od najnižšej po najvyššiu cenu</option>
+                <option value="price_desc" <?php if($sortOrder == 'price_desc') echo 'selected'; ?>>Od najvyššej po najnižšiu cenu</option>
+            </select>
             <button type="submit">Filter</button>
         </form>
     </div>       
